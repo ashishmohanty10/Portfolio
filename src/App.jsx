868fa82a35522ad7762942359db28dc6439643bc
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-02-24 00:27:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Skills.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Skills = () => {
+  return <div>Skills pages</div>;
+};
+
+export default Skills;
